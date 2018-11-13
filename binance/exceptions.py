f69979d6ad11e2f7bdeb549f8bb15ae79e786606
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-13 15:39:58 +0530

    Update exceptions.py

--- binance/exceptions.py
+++ binance/exceptions.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+class BinanceAPIException(Exception):
+
+    LISTENKEY_NOT_EXIST = '-1125'
+
+    def __init__(self, response):
+        self.status_code = 0
+        try:
+            json_res = response.json()
+        except ValueError:
+            self.message = 'Invalid JSON error message from Binance: {}'.format(response.text)
+        else:
+            self.code = json_res['code']
+            self.message = json_res['msg']
+        self.status_code = response.status_code
+        self.response = response
+        self.request = getattr(response, 'request', None)
+
+    def __str__(self):  # pragma: no cover
+        return 'APIError(code=%s): %s' % (self.code, self.message)
 
