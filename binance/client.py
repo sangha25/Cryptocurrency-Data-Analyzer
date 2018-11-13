d09f38de04a3db9eef925c955155337ba1c5a461
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-13 15:47:08 +0530

    Update client.py

--- binance/client.py
+++ binance/client.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import hashlib
+import hmac
+import requests
+import time
+from operator import itemgetter
+from .helpers import date_to_milliseconds, interval_to_milliseconds
+from .exceptions import BinanceAPIException, BinanceRequestException, BinanceWithdrawException
+
 
