0d4b3df7242dafa7d0577ace6256c68a7da80f86
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-13 15:48:00 +0530

    Update client.py

--- binance/client.py
+++ binance/client.py
@@ -9,4 +9,200 @@ from operator import itemgetter
 from .helpers import date_to_milliseconds, interval_to_milliseconds
 from .exceptions import BinanceAPIException, BinanceRequestException, BinanceWithdrawException
 
+class Client(object):
+
+    API_URL = 'https://api.binance.com/api'
+    WITHDRAW_API_URL = 'https://api.binance.com/wapi'
+    WEBSITE_URL = 'https://www.binance.com'
+    PUBLIC_API_VERSION = 'v1'
+    PRIVATE_API_VERSION = 'v3'
+    WITHDRAW_API_VERSION = 'v3'
+
+    SYMBOL_TYPE_SPOT = 'SPOT'
+
+    ORDER_STATUS_NEW = 'NEW'
+    ORDER_STATUS_PARTIALLY_FILLED = 'PARTIALLY_FILLED'
+    ORDER_STATUS_FILLED = 'FILLED'
+    ORDER_STATUS_CANCELED = 'CANCELED'
+    ORDER_STATUS_PENDING_CANCEL = 'PENDING_CANCEL'
+    ORDER_STATUS_REJECTED = 'REJECTED'
+    ORDER_STATUS_EXPIRED = 'EXPIRED'
+
+    KLINE_INTERVAL_1MINUTE = '1m'
+    KLINE_INTERVAL_3MINUTE = '3m'
+    KLINE_INTERVAL_5MINUTE = '5m'
+    KLINE_INTERVAL_15MINUTE = '15m'
+    KLINE_INTERVAL_30MINUTE = '30m'
+    KLINE_INTERVAL_1HOUR = '1h'
+    KLINE_INTERVAL_2HOUR = '2h'
+    KLINE_INTERVAL_4HOUR = '4h'
+    KLINE_INTERVAL_6HOUR = '6h'
+    KLINE_INTERVAL_8HOUR = '8h'
+    KLINE_INTERVAL_12HOUR = '12h'
+    KLINE_INTERVAL_1DAY = '1d'
+    KLINE_INTERVAL_3DAY = '3d'
+    KLINE_INTERVAL_1WEEK = '1w'
+    KLINE_INTERVAL_1MONTH = '1M'
+
+    SIDE_BUY = 'BUY'
+    SIDE_SELL = 'SELL'
+
+    ORDER_TYPE_LIMIT = 'LIMIT'
+    ORDER_TYPE_MARKET = 'MARKET'
+    ORDER_TYPE_STOP_LOSS = 'STOP_LOSS'
+    ORDER_TYPE_STOP_LOSS_LIMIT = 'STOP_LOSS_LIMIT'
+    ORDER_TYPE_TAKE_PROFIT = 'TAKE_PROFIT'
+    ORDER_TYPE_TAKE_PROFIT_LIMIT = 'TAKE_PROFIT_LIMIT'
+    ORDER_TYPE_LIMIT_MAKER = 'LIMIT_MAKER'
+
+    TIME_IN_FORCE_GTC = 'GTC'  # Good till cancelled
+    TIME_IN_FORCE_IOC = 'IOC'  # Immediate or cancel
+    TIME_IN_FORCE_FOK = 'FOK'  # Fill or kill
+
+    ORDER_RESP_TYPE_ACK = 'ACK'
+    ORDER_RESP_TYPE_RESULT = 'RESULT'
+    ORDER_RESP_TYPE_FULL = 'FULL'
+
+    def __init__(self, api_key, api_secret, requests_params=None):
+        """Binance API Client constructor
+
+        :param api_key: Api Key
+        :type api_key: str.
+        :param api_secret: Api Secret
+        :type api_secret: str.
+        :param requests_params: optional - Dictionary of requests params to use for all calls
+        :type requests_params: dict.
+
+        """
+
+        self.API_KEY = api_key
+        self.API_SECRET = api_secret
+        self.session = self._init_session()
+        self._requests_params = requests_params
+
+        # init DNS and SSL cert
+        self.ping()
+
+    def _init_session(self):
+
+        session = requests.session()
+        session.headers.update({'Accept': 'application/json',
+                                'User-Agent': 'binance/python',
+                                'X-MBX-APIKEY': self.API_KEY})
+        return session
+
+    def _create_api_uri(self, path, signed=True, version=PUBLIC_API_VERSION):
+        v = self.PRIVATE_API_VERSION if signed else version
+        return self.API_URL + '/' + v + '/' + path
+
+    def _create_withdraw_api_uri(self, path):
+        return self.WITHDRAW_API_URL + '/' + self.WITHDRAW_API_VERSION + '/' + path
+
+    def _create_website_uri(self, path):
+        return self.WEBSITE_URL + '/' + path
+
+    def _generate_signature(self, data):
+
+        ordered_data = self._order_params(data)
+        query_string = '&'.join(["{}={}".format(d[0], d[1]) for d in ordered_data])
+        m = hmac.new(self.API_SECRET.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256)
+        return m.hexdigest()
+
+    def _order_params(self, data):
+        """Convert params to list with signature as last element
+
+        :param data:
+        :return:
+
+        """
+        has_signature = False
+        params = []
+        for key, value in data.items():
+            if key == 'signature':
+                has_signature = True
+            else:
+                params.append((key, value))
+        # sort parameters by key
+        params.sort(key=itemgetter(0))
+        if has_signature:
+            params.append(('signature', data['signature']))
+        return params
+
+    def _request(self, method, uri, signed, force_params=False, **kwargs):
+
+        # set default requests timeout
+        kwargs['timeout'] = 10
+
+        # add our global requests params
+        if self._requests_params:
+            kwargs.update(self._requests_params)
+
+        data = kwargs.get('data', None)
+        if data and isinstance(data, dict):
+            kwargs['data'] = data
+        if signed:
+            # generate signature
+            kwargs['data']['timestamp'] = int(time.time() * 1000)
+            kwargs['data']['signature'] = self._generate_signature(kwargs['data'])
+
+        # sort get and post params to match signature order
+        if data:
+            # find any requests params passed and apply them
+            if 'requests_params' in kwargs['data']:
+                # merge requests params into kwargs
+                kwargs.update(kwargs['data']['requests_params'])
+                del(kwargs['data']['requests_params'])
+
+            # sort post params
+            kwargs['data'] = self._order_params(kwargs['data'])
+
+        # if get request assign data array to params value for requests lib
+        if data and (method == 'get' or force_params):
+            kwargs['params'] = kwargs['data']
+            del(kwargs['data'])
+
+        response = getattr(self.session, method)(uri, **kwargs)
+        return self._handle_response(response)
+
+    def _request_api(self, method, path, signed=False, version=PUBLIC_API_VERSION, **kwargs):
+        uri = self._create_api_uri(path, signed, version)
+
+        return self._request(method, uri, signed, **kwargs)
+
+    def _request_withdraw_api(self, method, path, signed=False, **kwargs):
+        uri = self._create_withdraw_api_uri(path)
+
+        return self._request(method, uri, signed, True, **kwargs)
+
+    def _request_website(self, method, path, signed=False, **kwargs):
+
+        uri = self._create_website_uri(path)
+
+        return self._request(method, uri, signed, **kwargs)
+
+    def _handle_response(self, response):
+        """Internal helper for handling API responses from the Binance server.
+        Raises the appropriate exceptions when necessary; otherwise, returns the
+        response.
+        """
+        if not str(response.status_code).startswith('2'):
+            raise BinanceAPIException(response)
+        try:
+            return response.json()
+        except ValueError:
+            raise BinanceRequestException('Invalid Response: %s' % response.text)
+
+    def _get(self, path, signed=False, version=PUBLIC_API_VERSION, **kwargs):
+        return self._request_api('get', path, signed, version, **kwargs)
+
+    def _post(self, path, signed=False, version=PUBLIC_API_VERSION, **kwargs):
+        return self._request_api('post', path, signed, version, **kwargs)
+
+    def _put(self, path, signed=False, version=PUBLIC_API_VERSION, **kwargs):
+        return self._request_api('put', path, signed, version, **kwargs)
+
+    def _delete(self, path, signed=False, version=PUBLIC_API_VERSION, **kwargs):
+        return self._request_api('delete', path, signed, version, **kwargs)
+
+
 
