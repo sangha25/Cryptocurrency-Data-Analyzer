92e195f6b9ad5ee4fb63bfa946116afc585c04fe
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-13 15:40:42 +0530

    Update exceptions.py

--- binance/exceptions.py
+++ binance/exceptions.py
@@ -41,3 +41,32 @@ class BinanceOrderException(Exception):
         return 'BinanceOrderException(code=%s): %s' % (self.code, self.message)
 
 
+class BinanceOrderMinAmountException(BinanceOrderException):
+
+    def __init__(self, value):
+        message = "Amount must be a multiple of %s" % value
+        super(BinanceOrderMinAmountException, self).__init__(-1013, message)
+
+
+class BinanceOrderMinPriceException(BinanceOrderException):
+
+    def __init__(self, value):
+        message = "Price must be at least %s" % value
+        super(BinanceOrderMinPriceException, self).__init__(-1013, message)
+
+
+class BinanceOrderMinTotalException(BinanceOrderException):
+
+    def __init__(self, value):
+        message = "Total must be at least %s" % value
+        super(BinanceOrderMinTotalException, self).__init__(-1013, message)
+
+
+class BinanceOrderUnknownSymbolException(BinanceOrderException):
+
+    def __init__(self, value):
+        message = "Unknown symbol %s" % value
+        super(BinanceOrderUnknownSymbolException, self).__init__(-1013, message)
+
+
+
