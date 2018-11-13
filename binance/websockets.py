53355674e97842429640d47863eb8cc01a4bc92b
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-13 15:34:27 +0530

    Update websockets.py

--- binance/websockets.py
+++ binance/websockets.py
@@ -13,3 +13,19 @@ from twisted.internet.error import ReactorAlreadyRunning
 
 from binance.client import Client
 
+
+class BinanceClientProtocol(WebSocketClientProtocol):
+
+    def onConnect(self, response):
+        # reset the delay after reconnecting
+        self.factory.resetDelay()
+
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            try:
+                payload_obj = json.loads(payload.decode('utf8'))
+            except ValueError:
+                pass
+            else:
+                self.factory.callback(payload_obj)
+
